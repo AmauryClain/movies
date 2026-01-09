de18c07d23a98c44be7b09d88af0a515807c0df8
Amaury Clain <amauryclain@gmail.com>
2026-01-09 10:34:02 +0100

    add (MovieDetail.jsx, routes for MovieDetail and MovieList)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Routes, Route } from "react-router";
 import MovieList from "./component/MovieList/MovieList.jsx";
+import MovieDetail from "./component/MovieDetail/MovieDetail.jsx";
 function App() {
   return (
-    <>
-      <MovieList />
-    </>
+    <Routes>
+      <Route path="/" element={<MovieList />} />
+      <Route path="/movies/:id" element={<MovieDetail />} />
+    </Routes>
   )
 }
 
