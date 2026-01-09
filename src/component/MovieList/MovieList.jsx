76018c1190bae6d7db696305cb9622276070244e
Amaury Clain <amauryclain@gmail.com>
2026-01-09 09:06:19 +0100

    add (MovieCard.jsx, Basic CSS for MovieCard and MovieList

--- src/component/MovieList/MovieList.jsx
+++ src/component/MovieList/MovieList.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
+import MovieCard from "../MovieCard/MovieCard";
+import styles from './MovieList.module.css';
 function MovieList() {
     const [movies, setMovies] = useState([]);
     const API_KEY = import.meta.env.VITE_API_KEY;
     useEffect(() => {
-        fetch("https://api.themoviedb.org/3/movie/popular?api_key=" + API_KEY)
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=" + API_KEY+"&language=fr")
             .then((response) => response.json())
             .then((data) => setMovies(data.results ?? []))
             .catch((error) => console.error("Error fetching movies:", error));
@@ -11,14 +13,18 @@ function MovieList() {
     return (
         <div>
             <h1>Movie List</h1>
-            <ul>
+            <div className={styles.list}>
                 {movies.map((movie) => (
-                    <li key={movie.id}>
-                        <h2>{movie.title}</h2>
-                        <p>{movie.overview}</p>
-                    </li>
+                    <MovieCard 
+                        id = {movie.id}
+                        img = {"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                        title = {movie.title}
+                        overview = {movie.overview}
+                        releaseDate = {movie.release_date}
+                        voteAverage = {movie.vote_average}
+                    />
                 ))}
-            </ul>
+            </div>
         </div>
     );
 }
